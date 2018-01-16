0ee6c49b717f308693555b8afecfc6796ef337d0
Tzmm.987 <chengjiaoyang1016@163.com>
2018-01-16 16:21:13 +0800

    调试

--- js/index/headerNav.js
+++ js/index/headerNav.js
@@ -5,12 +5,12 @@ require(['../require.config'], function () {
         $(document).mousemove(function (event) {
             // header显示隐藏
             if (event.pageY < 30) {
-                console.log('小于5');
+
                 $('header').stop().animate({
                     top: "0"
                 }, 100);
             } else if (event.pageY > 200) {
-                console.log(0);
+
                 $('header').stop().animate({
                     top: "-4rem"
                 }, 100);
@@ -18,12 +18,12 @@ require(['../require.config'], function () {
             // aside显示隐藏
 
             if (event.pageX < 30) {
-                console.log('小于5');
+              
                 $('aside').stop().animate({
                     left: "0"
                 }, 100);
             } else if (event.pageX > 200) {
-                console.log(0);
+
                 $('aside').stop().animate({
                     left: "-13rem"
                 }, 100);
